a40788af080fa3a18446aecc0118702e70a4bfd5
solomondworkin <solomon.dworkin@gmail.com>
2021-03-06 11:31:29 -0600

    switching to networkx structure

--- generations.py
+++ generations.py
@@ -1,24 +1,26 @@
 import sys, os
-from treelib import Node, Tree
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class FamilyTree(object):
     def __init__(self, object):
         self.num_generations = object.get("num_generations")
         self.start_year = object.get("start_year")
-        self.tree = Tree()
+        self.tree = nx.Graph()
 
     def individual(self):
-        self.tree.create_node("new_child0", "new_child0")
+        self.tree.add_node(0, name='new_node0')
         i = 1
         while i <= self.num_generations:
-            self.tree.create_node("new_child" + str(i), "new_child" + str(i), parent = "new_child" + str(i-1))
+            self.tree.add_node(i, name = 'new_node' + str(i))
+            self.tree.add_edge(0, i)
             i+=1
 
     def tree_run(self):
 
         self.individual()
-        self.tree.show()
-    
+        plt.subplot(122)
+        nx.draw(self.tree, with_labels=True, font_weight='bold') 
 
 
 
