adde82c0a7c26aa6676abcd1efda15fdc6f4cdde
solomondworkin <solomon.dworkin@gmail.com>
2021-03-06 11:04:53 -0600

    progress on tree

--- generations.py
+++ generations.py
@@ -1,11 +1,24 @@
+import sys, os
+from treelib import Node, Tree
 
+class FamilyTree(object):
+    def __init__(self, object):
+        self.num_generations = object.get("num_generations")
+        self.start_year = object.get("start_year")
+        tree = Tree()
 
-class ():
-    def __init__():
-        pass
+    def individual(self):
+        tree.create_node("new_child0")
+        i = 1
+        while i <= num_generations:
+            tree.create_node("new_child" + str(i), parent = "new_child" + str(i-1))
+            i+=1
 
-    def load_
-        
+    def tree_run(self):
+
+        self.individual()
+        self.tree.show()
+    
 
 
 
