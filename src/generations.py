366b9b7631c63b799c1f3611cd3a9f1ccf9f6044
Solomon Dworkin <solomondworkin@solomons-mbp-2.lan>
2021-03-13 18:41:53 -0500

    structure now finished

--- src/generations.py
+++ src/generations.py
@@ -3,6 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import csv
 import random
+import copy
 from names import Names
 
 
@@ -17,14 +18,18 @@ class FamilyTree(object):
         mother_first_name = object.get("mother")[0]
         mother_last_name = object.get("mother")[1]
         mother_age = object.get("mother")[2]
-        self.tree.add_node(0, first_name = mother_first_name, last_name = mother_last_name, age = mother_age, gender = "f", alive = "y")
-        self.tree.add_node(1, first_name = father_first_name, last_name = father_last_name, age = father_age, gende = "m", alive = "y")
+        self.tree.add_node(0, first_name = mother_first_name, last_name = mother_last_name, age = mother_age, gender = "f", alive = True)
+        self.tree.add_node(1, first_name = father_first_name, last_name = father_last_name, age = father_age, gender = "m", alive = True)
         self.tree.add_edge(0,1, relation = "marriage")
 
         self.fem_names = Names().fem_names
         self.masc_names = Names().masc_names
         self.surnames = Names().surnames
         self.person_id = 2
+
+        # self.temp_tree is a dummy tree because it's impossible to alter the original tree
+        # as we iterate through it
+        self.temp_tree = copy.deepcopy(self.tree)
 
     def r_first_name(self, gender):
         '''
@@ -74,45 +79,68 @@ class FamilyTree(object):
         if relationship == "marriage":
             # need to fix age, gender should be opposite spouse, not random
             new_age = 15 + random.choice(range(20))
-            new_gender = nx.get_node_attributes("gender")[relations[0]]
-            self.tree.add_node(self.person_id, first_name = self.r_first_name(new_gender), last_name = self.r_last_name, age = new_age, gender = new_gender, alive = "y" )
-            self.tree.add_edge(relations[0], 0)
+            gender = nx.get_node_attributes(self.tree, "gender")[relations[0]]
+            if gender == "f":
+                new_gender = "m"
+            if gender == "m":
+                new_gender = "f"
+            print(gender)
+            print(new_gender)
+            self.temp_tree.add_node(self.person_id, first_name = self.r_first_name(new_gender), last_name = self.r_last_name, age = new_age, gender = new_gender, alive = True)
+            self.temp_tree.add_edge(relations[0], self.person_id, relation = "marriage")
 
-        if relationship == "birth":
+        if relationship == "child":
             # need to fix last name to be the fathers last name
             # new_age = 0
             # new_gender = random.choice(["f", "m"])
             # new_last_name = 
-            self.tree.add_node(self.person_id, first_name = self.r_first_name(new_gender), last_name = self.r_last_name, age = 0, gender = new_gender, alive = "y")
+            self.temp_tree.add_node(self.person_id, first_name = self.r_first_name(new_gender), last_name = self.r_last_name, age = 0, gender = new_gender, alive = True)
             for i in relations:
-                 self.tree.add_edge(i, 0)
+                 self.temp_tree.add_edge(i, self.person_id, relation = "child")
 
         self.person_id += 1
 
     def prob_dead(self):
-
+        # TODO:
         pass
 
     def prob_marry(self):
-
+        #TODO:
         pass
 
     def prob_birth(self):
-
+        #TODO:
         pass
 
         
-    def year(self):
+    def one_year(self):
 
         '''
         simulates one year, modifying the self.tree attribute
         '''
+        people = list(self.tree.nodes)
+        alive = nx.get_node_attributes(self.tree, "alive")
+        seen_edges = []
 
-        tree = self.tree
-        print(tree.get_edge_data(*(0,1)))
-       
-        #names = nx.get_node_attributes(tree, 'name')
-
+        for person in people:
+            if not alive[person]:
+                print("dead")
+                break
+            # TODO: need to increment all ages by 1
+            # all relationships of person  
+            relations = self.tree.edges(person)
+            # delivers babies to married families that are expecting
+            not_married = len(relations)
+            for relationship in relations:
+                if self.tree.edges[relationship]["relation"] == "marriage" and relationship not in seen_edges:
+                    self.individual("child", relationship)
+                    seen_edges.append(relationship[::-1])
+                if self.tree.edges[relationship]["relation"] != "marriage":
+                    print("here")
+                    not_married -= 1
+            if not_married == 0:
+                self.individual("marriage", [person])
+            
 
  
         # visit each node in tree
@@ -128,7 +156,9 @@ class FamilyTree(object):
         # determine probability that they die
 
 
-        # increment year by 1
+        self.year += 1
+        self.tree = copy.deepcopy(self.temp_tree)
+        print(self.tree.nodes)
 
     def tree_run(self):
         '''
@@ -136,9 +166,11 @@ class FamilyTree(object):
         '''
         self.r_first_name("f")
         self.r_last_name()
-
+        self.one_year()
+        self.one_year()
+        self.one_year()
+        print(nx.get_node_attributes(self.tree, "first_name"))
         #self.individual(1,2)
-        #self.year()
         plt.subplot(122)
         nx.draw(self.tree, with_labels=True, font_weight='bold') 
         plt.savefig('test.png')
